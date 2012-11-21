d3ee05abe4fff92163e92a35d4b3c2e318aef06a
Tim Booher <tim@theboohers.org>
2012-11-20 21:18:26 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BooherFamily::Application.config.secret_token = 'e556c35a1bac8d2ccb4d873f753cd9b111e736161c0f3f661663e4c89329ad337577495e2c3a2a8dc37c13391a31ac12e55a3ce66ac4fef28be854a631aaae78'
