706ae850b08c0cafeb16e808d287d58a5f2cd30a
Tim Booher <tim@theboohers.org>
2012-11-26 06:28:16 -0500

    update migrations

--- db/migrate/20121125162313_add_references_to_trips.rb
+++ db/migrate/20121125162313_add_references_to_trips.rb
@@ -2,7 +2,7 @@ class AddReferencesToTrips < ActiveRecord::Migration
   def change
     add_column :trip_meals, :trip_day_id, :integer
     add_index  :trip_meals, :trip_day_id
-    remove_column :trip_meals, :user_id, :integer
+    remove_column :trip_meals, :user_id
     add_column :trip_days, :trip_id, :integer
     add_index :trip_days, :trip_id
     # remove_index :trip_meals, :user_id
