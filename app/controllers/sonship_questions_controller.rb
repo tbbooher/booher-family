3765b2c744a70d44fe7b970a318f1ddf68fccce1
Tim Booher <tim@theboohers.org>
2013-01-30 07:12:20 -0500

    updated questions

--- app/controllers/sonship_questions_controller.rb
+++ app/controllers/sonship_questions_controller.rb
@@ -1,3 +1,11 @@
 class SonshipQuestionsController < InheritedResources::Base
   belongs_to :sonship_lesson
+
+  def update
+    update!{ sonship_lesson_sonship_questions_path(@sonship_lesson) }
+  end
+
+  def create
+    create! { sonship_lesson_sonship_questions_path(@sonship_lesson) }
+  end
 end
