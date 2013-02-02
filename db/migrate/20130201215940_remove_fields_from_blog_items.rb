1356112200aa21f316380d4e227e1edddeeb57c2
Tim Booher <tim@theboohers.org>
2013-02-01 20:00:05 -0500

    updated

--- db/migrate/20130201215940_remove_fields_from_blog_items.rb
+++ db/migrate/20130201215940_remove_fields_from_blog_items.rb
@@ -1,6 +1,6 @@
 class RemoveFieldsFromBlogItems < ActiveRecord::Migration
   def change
-    remove_column :blogs, :user_id_id
+    #remove_column :blogs, :user_id_id
     remove_column :posts, :tag_id_id
     remove_column :posts, :user_id_id
   end
