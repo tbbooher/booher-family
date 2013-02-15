0fa6141cecbb1c0643125b2940ed327c1d99a993
Tim Booher <tim@theboohers.org>
2013-02-14 22:19:49 -0500

    removed comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  acts_as_nested_set :scope => [:commentable_id, :commentable_type]
+  #acts_as_nested_set :scope => [:commentable_id, :commentable_type]
 
   validates_presence_of :body
   validates_presence_of :user
