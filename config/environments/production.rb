abbff6ca3443d1f03622c0c42793ae1d106fb609
Tim Booher <tim@theboohers.org>
2013-09-28 11:01:53 -0400

    updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ BooherFamily::Application.configure do
   config.action_mailer.default_url_options = { :host => 'booher-life.herokuapp.com' }
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
